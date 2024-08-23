6545eb3eb6b0bb4249e71e3b4a8ca91df9e09566
Yousof Zarepoor <yuveloper@yahoo.com>
2024-08-23 11:33:28 +0330

    initial commit

--- blog1/views.py
+++ blog1/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import post
 
-def home(request):
-    return render(request,'home.html',{})
+#def home(request):
+#    return render(request,'home.html',{})
+class HomeView (ListView):
+    model = post
+    template_name='home.html'
+class PostView (DetailView):
+    model=post
+    template_name='postview.html'
